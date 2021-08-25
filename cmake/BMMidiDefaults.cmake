4d586c7782096ed7c8215a52ed413f57799143be
Eric Barndollar <eric@EricWBarndollar.com>
2021-08-25 14:57:58 -0400

    CMake project skeleton

---
+++ cmake/BMMidiDefaults.cmake
@@ -0,0 +1,59 @@
+if(NOT BMMIDI_IS_TOP_LEVEL_PROJECT)
+  if(CMAKE_CXX_STANDARD LESS 14)
+    message(FATAL_ERROR
+        "BMMidi requires CMAKE_CXX_STANDARD >= 14 (got: ${CMAKE_CXX_STANDARD})")
+  endif()
+
+  return()  # Don't set these defaults if this isn't the top-level project.
+endif()
+
+# The defaults below apply only when BMMidi is the top-level CMake project
+# (i.e. when developing it directly as opposed to using it as a library within
+# another project).
+
+include(CheckCXXCompilerFlag)
+
+# Default to C++14 standard.
+set(CMAKE_CXX_STANDARD 14)
+set(CMAKE_CXX_STANDARD_REQUIRED YES)
+set(CMAKE_CXX_EXTENSIONS NO)
+
+# Default all compilers to hiding symbols by default.
+set(CMAKE_C_VISIBILITY_PRESET hidden)
+set(CMAKE_CXX_VISIBILITY_PRESET hidden)
+set(CMAKE_VISIBILITY_INLINES_HIDDEN TRUE)
+
+# Prefer finding package configs to Find<pkg> modules, and isolate builds
+# from user/system package registries.
+set(CMAKE_FIND_PACKAGE_PREFER_CONFIG TRUE)
+set(CMAKE_EXPORT_NO_PACKAGE_REGISTRY TRUE)
+set(CMAKE_FIND_USE_PACKAGE_REGISTRY FALSE)
+set(CMAKE_FIND_USE_SYSTEM_PACKAGE_REGISTRY FALSE)
+
+# If available, split debug symbols into separate .dwo files for debug builds.
+check_cxx_compiler_flag("-gsplit-dwarf" HAVE_SPLIT_DWARF)
+if(HAVE_SPLIT_DWARF)
+  add_compile_options("$<$<CONFIG:Debug>:-gsplit-dwarf>")
+  add_compile_options("$<$<CONFIG:RelWithDebInfo>:-gsplit-dwarf>")
+endif()
+
+# Collect binaries under stage/ subdirectory, unless a parent project overrides
+# this default.
+include(GNUInstallDirs)
+if(NOT CMAKE_RUNTIME_OUTPUT_DIRECTORY)
+  set(CMAKE_RUNTIME_OUTPUT_DIRECTORY
+      ${BMMidi_BINARY_DIR}/stage/${CMAKE_INSTALL_BINDIR})
+endif()
+if(NOT CMAKE_LIBRARY_OUTPUT_DIRECTORY)
+  set(CMAKE_LIBRARY_OUTPUT_DIRECTORY
+      ${BMMidi_BINARY_DIR}/stage/${CMAKE_INSTALL_LIBDIR})
+endif()
+if(NOT CMAKE_ARCHIVE_OUTPUT_DIRECTORY)
+  set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY
+      ${BMMidi_BINARY_DIR}/stage/${CMAKE_INSTALL_LIBDIR})
+endif()
+
+# Discover Google Test list of tests at test time, rather than post build.
+if(BMMIDI_ENABLE_TESTS)
+  set(CMAKE_GTEST_DISCOVER_TESTS_DISCOVERY_MODE PRE_TEST)
+endif()
---
+++ cmake/BMMidiTargets.cmake
@@ -0,0 +1,86 @@
+include(GNUInstallDirs)
+
+## Adds header-only library with BMMidi project defaults.
+##
+## Creates target named BMMidi_${name} and alias of
+## BMMidi::${name}.
+##
+## All remaining arguments are passed as sources to target_sources().
+function(bmmidi_header_library name)
+  add_library(BMMidi_${name} INTERFACE)
+  add_library(BMMidi::${name} ALIAS BMMidi_${name})
+  set_target_properties(BMMidi_${name} PROPERTIES
+      EXPORT_NAME ${name})
+
+  target_sources(BMMidi_${name} INTERFACE ${ARGN})
+
+  # Always include the src/ dir as a base include path.
+  target_include_directories(BMMidi_${name}
+      INTERFACE ${BMMidi_SOURCE_DIR}/src)
+
+  target_compile_features(BMMidi_${name} INTERFACE cxx_std_14)
+endfunction()
+
+## Configures target compile options to enable this project's standard set of
+## warnings for the BMMidi_${name} target.
+function(bmmidi_enable_warnings name)
+  # Enable strict warnings.
+  if (MSVC)
+    target_compile_options(BMMidi_${name}
+        PRIVATE /W4)
+  else()
+    target_compile_options(BMMidi_${name}
+        PRIVATE -Wall -Wextra -pedantic)
+  endif()
+endfunction()
+
+## Adds library with BMMidi project defaults.
+##
+## Creates target named BMMidi_${name} and alias of
+## BMMidi::${name}.
+##
+## All remaining arguments are passed to add_library().
+function(bmmidi_library name)
+  add_library(BMMidi_${name} ${ARGN})
+  add_library(BMMidi::${name} ALIAS BMMidi_${name})
+  set_target_properties(BMMidi_${name} PROPERTIES
+      OUTPUT_NAME ${name}
+      EXPORT_NAME ${name})
+
+  # Always include the src/ dir as a base include path.
+  target_include_directories(BMMidi_${name}
+      PUBLIC ${BMMidi_SOURCE_DIR}/src)
+
+  target_compile_features(BMMidi_${name} PUBLIC cxx_std_14)
+  bmmidi_enable_warnings(${name})
+
+  # TODO: Configure installation, if necessary.
+endfunction()
+
+## If testing for this project is enabled, adds test executable and matching
+## CTest test with BMMidi project defaults. Creates executable named
+## BMMidi_${name}.
+##
+## All remaining arguments are passed to add_executable().
+function(bmmidi_gtest name)
+  if(NOT BMMIDI_ENABLE_TESTS)
+    # Project testing off, so bmmidi_gtest() should never have been invoked.
+    message(FATAL_ERROR "Must guard test targets with BMMIDI_ENABLE_TESTS")
+  endif()
+
+  include(GoogleTest)
+
+  add_executable(BMMidi_${name} ${ARGN})
+
+  # Always include the src/ dir as a base include path.
+  target_include_directories(BMMidi_${name}
+      PUBLIC ${BMMidi_SOURCE_DIR}/src)
+
+  target_compile_features(BMMidi_${name} PUBLIC cxx_std_14)
+  bmmidi_enable_warnings(${name})
+
+  target_link_libraries(BMMidi_${name}
+      PRIVATE gtest gmock gtest_main)
+  gtest_discover_tests(BMMidi_${name}
+      WORKING_DIRECTORY ${BMMidi_BINARY_DIR}/stage/${CMAKE_INSTALL_BINDIR})
+endfunction()
