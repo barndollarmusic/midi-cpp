621a4c991ce4d1fe7e1c804393e194e0e11dc021
Eric Barndollar <eric@EricWBarndollar.com>
2021-08-26 18:19:52 -0400

    MsgView, MsgRef

--- src/bmmidi/msg.hpp
+++ src/bmmidi/msg.hpp
@@ -12,6 +12,140 @@
 namespace bmmidi {
 
 /**
+ * A read-only view of a MIDI message stored as contiguous bytes (which
+ * must outlive this reference object).
+ *
+ * For example, this can refer to a Msg<N> instance, a SysExMsg, or to bytes
+ * managed by any other MIDI framework.
+ *
+ * Make sure MIDI data this is referring to does NOT contain any interleaved
+ * System Realtime message bytes, which can occur at any point in a raw MIDI
+ * byte stream.
+ */
+class MsgView {
+public:
+  /**
+   * Constructs a new MsgView pointing to contiguous MIDI data bytes, starting
+   * from Status byte byte[0] and spanning through last data byte (inclusive)
+   * of bytes[numBytes - 1].
+   *
+   * The referenced MIDI data must outlive this MsgView instance.
+   */
+  explicit MsgView(const std::uint8_t* bytes, int numBytes)
+      : bytes_{bytes}, numBytes_{numBytes} {
+    assert(bytes != nullptr);
+    assert(numBytes >= 1);
+    assert((type() == MsgType::kSystemExclusive)
+        || (numBytes == 1 + status().numDataBytes()));
+  }
+
+  /** Returns type of this message, as determined by its Status byte. */
+  MsgType type() const { return status().type(); }
+
+  /** Returns Status byte. */
+  Status status() const { return Status{bytes_[0]}; }
+
+  /**
+   * Returns [0, 127] value of the 1st data byte. Error to call (fails
+   * debug assertion) if this message does not have any data bytes.
+   */
+  DataValue data1() const {
+    assert(status().numDataBytes() >= 1);
+    return DataValue{static_cast<std::int8_t>(bytes_[1])};
+  }
+
+  /**
+   * Returns [0, 127] value of the 2nd data byte. Error to call (fails
+   * debug assertion) if this message does not have a 2nd data byte.
+   */
+  DataValue data2() const {
+    assert(status().numDataBytes() >= 2);
+    return DataValue{static_cast<std::int8_t>(bytes_[2])};
+  }
+
+  /**
+   * Returns the number of bytes that this message contains, including the
+   * initial status byte.
+   */
+  int numBytes() const { return numBytes_; }
+
+  /** Returns read-only pointer to first byte. */
+  const std::uint8_t* rawBytes() const { return bytes_; }
+
+private:
+  const std::uint8_t* bytes_;
+  int numBytes_;
+};
+
+/** Alias for a timestamped read-only view of a MIDI message. */
+using TimedMsgView = Timed<MsgView>;
+
+/**
+ * A read/write reference to a MIDI message stored as contiguous bytes (which
+ * must outlive this reference object).
+ *
+ * For example, this can refer to a Msg<N> instance, a SysExMsg, or to bytes
+ * managed by any other MIDI framework.
+ *
+ * Make sure MIDI data this is referring to does NOT contain any interleaved
+ * System Realtime message bytes, which can occur at any point in a raw MIDI
+ * byte stream.
+ */
+class MsgRef : public MsgView {
+public:
+  /**
+   * Constructs a new MsgRef pointing to contiguous MIDI data bytes, starting
+   * from Status byte byte[0] and spanning through last data byte (inclusive)
+   * of bytes[numBytes - 1].
+   *
+   * The referenced MIDI data must outlive this MsgRef instance.
+   */
+  explicit MsgRef(std::uint8_t* bytes, int numBytes)
+      : MsgView{bytes, numBytes} {}
+
+  /** Updates Status byte to the given value. */
+  void setStatus(Status status) {
+    rawBytes()[0] = status.value();
+  }
+
+  /**
+   * Updates 1st data byte to the given value. Error to call (fails
+   * debug assertion) if this message does not have any data bytes.
+   */
+  void setData1(DataValue data1) {
+    assert(status().numDataBytes() >= 1);
+    rawBytes()[1] = static_cast<std::uint8_t>(data1.value());
+  }
+
+  /**
+   * Updates 2nd data byte to the given value. Error to call (fails
+   * debug assertion) if this message does not have a 2nd data byte.
+   */
+  void setData2(DataValue data2) {
+    assert(status().numDataBytes() >= 2);
+    rawBytes()[2] = static_cast<std::uint8_t>(data2.value());
+  }
+
+  /**
+   * Returns read/write pointer to first byte; careful if mutating raw bytes
+   * directly, since it is of course possible to mutate into something that
+   * does not represent a valid MIDI message (which can cause undefined behavior
+   * for this bmmidi library).
+   */
+  std::uint8_t* rawBytes() {
+    // NOTE: This is safe only because we know MsgRef was constructed with a
+    // non-const std::uint8_t*. Not storing the non-const pointer separately,
+    // since that would be redundant.
+    return const_cast<std::uint8_t*>(MsgView::rawBytes());
+  }
+};
+
+/** Alias for a timestamped read/write reference to a MIDI message. */
+using TimedMsgRef = Timed<MsgRef>;
+
+// TODO: Add more specific MsgView/Ref classes.
+
+/**
  * Base class for an N-byte MIDI message that stores its own bytes contiguously.
  *
  * Memory layout is packed bytes, so it should be safe to use reinterpret_cast<>
@@ -25,20 +159,20 @@ public:
   /** The # of bytes (N) this message contains. */
   static constexpr std::size_t kNumBytes = N;
 
-  /** Read-only raw data array type. */
-  using ConstDataArray = const std::uint8_t[N];
+  /** Read-only raw byte array type. */
+  using ConstByteArray = const std::uint8_t[N];
 
   template<typename = std::enable_if_t<N == 1>>
   explicit Msg(Status status)
-      : data_{status.value()} {}
+      : bytes_{status.value()} {}
 
   template<typename = std::enable_if_t<N == 2>>
   explicit Msg(Status status, DataValue data1)
-      : data_{status.value(), static_cast<std::uint8_t>(data1.value())} {}
+      : bytes_{status.value(), static_cast<std::uint8_t>(data1.value())} {}
 
   template<typename = std::enable_if_t<N == 3>>
   explicit Msg(Status status, DataValue data1, DataValue data2)
-      : data_{status.value(),
+      : bytes_{status.value(),
               static_cast<std::uint8_t>(data1.value()),
               static_cast<std::uint8_t>(data2.value())} {}
 
@@ -46,25 +180,31 @@ public:
   MsgType type() const { return status().type(); }
 
   /** Returns Status byte (first byte of this MIDI message). */
-  Status status() const { return Status{data_[0]}; }
+  Status status() const { return Status{bytes_[0]}; }
 
   /** Returns [0, 127] value of the 1st data byte. */
   template<typename = std::enable_if_t<N >= 2>>
   DataValue data1() const {
-    return DataValue{static_cast<std::int8_t>(data_[1])};
+    return DataValue{static_cast<std::int8_t>(bytes_[1])};
   }
 
   /** Returns [0, 127] value of the 2nd data byte. */
   template<typename = std::enable_if_t<N >= 3>>
   DataValue data2() const {
-    return DataValue{static_cast<std::int8_t>(data_[2])};
+    return DataValue{static_cast<std::int8_t>(bytes_[2])};
   }
 
-  /** Returns read-only reference to raw data array. */
-  ConstDataArray& rawData() const { return data_; }
+  /** Returns read-only reference to raw byte array (including Status byte). */
+  ConstByteArray& rawBytes() const { return bytes_; }
+
+  /** Returns read-only view of this message. */
+  MsgView view() const { return MsgView{bytes_, N}; }
+
+  /** Returns read-write reference to this message. */
+  MsgRef ref() { return MsgRef{bytes_, N}; }
 
 private:
-  std::uint8_t data_[N];
+  std::uint8_t bytes_[N];
 };
 
 // Ensure compiler keeps these structs packed.
@@ -95,6 +235,8 @@ inline bool operator!=(Msg<N> lhs, Msg<N> rhs) { return !(lhs == rhs); }
 /** Alias for a timestamped N-byte MIDI message. */
 template<std::size_t N>
 using TimedMsg = Timed<Msg<N>>;
+
+// TODO: Add SysExMsg and other more specific message classes.
 
 }  // namespace bmmidi
 
--- src/bmmidi/msg_test.cpp
+++ src/bmmidi/msg_test.cpp
@@ -159,17 +159,17 @@ TEST(Msg, ProvidesReadOnlyRawArrayAccess) {
   // One byte version:
   bmmidi::Msg<1> oscTuneRequest{
       bmmidi::Status::system(bmmidi::MsgType::kOscillatorTuneRequest)};
-  EXPECT_THAT(sizeof(oscTuneRequest.rawData()), Eq(1));
-  EXPECT_THAT(oscTuneRequest.rawData()[0], Eq(std::uint8_t{0xF6}));
+  EXPECT_THAT(sizeof(oscTuneRequest.rawBytes()), Eq(1));
+  EXPECT_THAT(oscTuneRequest.rawBytes()[0], Eq(std::uint8_t{0xF6}));
 
   // Two byte version:
   bmmidi::Msg<2> programChangeCh13{
       bmmidi::Status::channelVoice(bmmidi::MsgType::kProgramChange,
                                    bmmidi::Channel::index(12)),
       bmmidi::DataValue{57}};
-  EXPECT_THAT(sizeof(programChangeCh13.rawData()), Eq(2));
-  EXPECT_THAT(programChangeCh13.rawData()[0], Eq(std::uint8_t{0xCC}));
-  EXPECT_THAT(programChangeCh13.rawData()[1], Eq(std::uint8_t{57}));
+  EXPECT_THAT(sizeof(programChangeCh13.rawBytes()), Eq(2));
+  EXPECT_THAT(programChangeCh13.rawBytes()[0], Eq(std::uint8_t{0xCC}));
+  EXPECT_THAT(programChangeCh13.rawBytes()[1], Eq(std::uint8_t{57}));
 
   // Three byte version:
   bmmidi::Msg<3> controlChangeCh10{
@@ -177,10 +177,184 @@ TEST(Msg, ProvidesReadOnlyRawArrayAccess) {
                                    bmmidi::Channel::index(9)),
       bmmidi::controlToDataValue(bmmidi::Control::kExpression),
       bmmidi::DataValue{76}};
-  EXPECT_THAT(sizeof(controlChangeCh10.rawData()), Eq(3));
-  EXPECT_THAT(controlChangeCh10.rawData()[0], Eq(std::uint8_t{0xB9}));
-  EXPECT_THAT(controlChangeCh10.rawData()[1], Eq(std::uint8_t{11}));
-  EXPECT_THAT(controlChangeCh10.rawData()[2], Eq(std::uint8_t{76}));
+  EXPECT_THAT(sizeof(controlChangeCh10.rawBytes()), Eq(3));
+  EXPECT_THAT(controlChangeCh10.rawBytes()[0], Eq(std::uint8_t{0xB9}));
+  EXPECT_THAT(controlChangeCh10.rawBytes()[1], Eq(std::uint8_t{11}));
+  EXPECT_THAT(controlChangeCh10.rawBytes()[2], Eq(std::uint8_t{76}));
 }
+
+TEST(MsgView, WorksForStandaloneBytes) {
+  // Control Change, channel 13, CC #1 (Mod Wheel), value 37.
+  const std::uint8_t srcBytes[] = {0xBC, 0x01, 0x25};
+  bmmidi::MsgView view{srcBytes, sizeof(srcBytes)};
+
+  // Read-only accessors:
+  EXPECT_THAT(view.type(), Eq(bmmidi::MsgType::kControlChange));
+  EXPECT_THAT(view.status(),
+      Eq(bmmidi::Status::channelVoice(bmmidi::MsgType::kControlChange,
+                                      bmmidi::Channel::index(12))));
+  EXPECT_THAT(view.data1(), Eq(bmmidi::DataValue{1}));
+  EXPECT_THAT(view.data2(), Eq(bmmidi::DataValue{37}));
+  EXPECT_THAT(view.numBytes(), Eq(3));
+
+  EXPECT_THAT(view.rawBytes()[0], Eq(0xBC));
+  EXPECT_THAT(view.rawBytes()[1], Eq(0x01));
+  EXPECT_THAT(view.rawBytes()[2], Eq(0x25));
+}
+
+TEST(MsgView, WorksForMsg) {
+  // Control Change, channel 13, CC #1 (Mod Wheel), value 37.
+  bmmidi::Msg<3> ccCh13Mod37{
+      bmmidi::Status::channelVoice(bmmidi::MsgType::kControlChange,
+                                   bmmidi::Channel::index(12)),
+      bmmidi::controlToDataValue(bmmidi::Control::kModWheel),
+      bmmidi::DataValue{37}};
+  auto view = ccCh13Mod37.view();
+
+  // Read-only accessors:
+  EXPECT_THAT(view.type(), Eq(bmmidi::MsgType::kControlChange));
+  EXPECT_THAT(view.status(),
+      Eq(bmmidi::Status::channelVoice(bmmidi::MsgType::kControlChange,
+                                      bmmidi::Channel::index(12))));
+  EXPECT_THAT(view.data1(), Eq(bmmidi::DataValue{1}));
+  EXPECT_THAT(view.data2(), Eq(bmmidi::DataValue{37}));
+  EXPECT_THAT(view.numBytes(), Eq(3));
+
+  EXPECT_THAT(view.rawBytes()[0], Eq(0xBC));
+  EXPECT_THAT(view.rawBytes()[1], Eq(0x01));
+  EXPECT_THAT(view.rawBytes()[2], Eq(0x25));
+}
+
+TEST(MsgRef, WorksForStandaloneBytes) {
+  // Control Change, channel 13, CC #1 (Mod Wheel), value 37.
+  std::uint8_t srcBytes[] = {0xBC, 0x01, 0x25};
+  bmmidi::MsgRef ref{srcBytes, sizeof(srcBytes)};
+
+  // Read-only accessors are still available:
+  EXPECT_THAT(ref.type(), Eq(bmmidi::MsgType::kControlChange));
+  EXPECT_THAT(ref.status(),
+      Eq(bmmidi::Status::channelVoice(bmmidi::MsgType::kControlChange,
+                                      bmmidi::Channel::index(12))));
+  EXPECT_THAT(ref.data1(), Eq(bmmidi::DataValue{1}));
+  EXPECT_THAT(ref.data2(), Eq(bmmidi::DataValue{37}));
+  EXPECT_THAT(ref.numBytes(), Eq(3));
+
+  EXPECT_THAT(ref.rawBytes()[0], Eq(0xBC));
+  EXPECT_THAT(ref.rawBytes()[1], Eq(0x01));
+  EXPECT_THAT(ref.rawBytes()[2], Eq(0x25));
+
+  // Mutations:
+  ref.setData2(bmmidi::DataValue{126});
+  EXPECT_THAT(ref.status(),
+      Eq(bmmidi::Status::channelVoice(bmmidi::MsgType::kControlChange,
+                                      bmmidi::Channel::index(12))));
+  EXPECT_THAT(ref.data1(), Eq(bmmidi::DataValue{1}));
+  EXPECT_THAT(ref.data2(), Eq(bmmidi::DataValue{126}));
+
+  ref.setData1(bmmidi::DataValue{0x45});
+  EXPECT_THAT(ref.status(),
+      Eq(bmmidi::Status::channelVoice(bmmidi::MsgType::kControlChange,
+                                      bmmidi::Channel::index(12))));
+  EXPECT_THAT(ref.data1(), Eq(bmmidi::DataValue{69}));
+  EXPECT_THAT(ref.data2(), Eq(bmmidi::DataValue{126}));
+
+  ref.setStatus(
+      bmmidi::Status::channelVoice(bmmidi::MsgType::kPolyphonicKeyPressure,
+                                   bmmidi::Channel::index(15)));
+  EXPECT_THAT(ref.status(),
+      Eq(bmmidi::Status::channelVoice(bmmidi::MsgType::kPolyphonicKeyPressure,
+                                      bmmidi::Channel::index(15))));
+  EXPECT_THAT(ref.data1(), Eq(bmmidi::DataValue{69}));
+  EXPECT_THAT(ref.data2(), Eq(bmmidi::DataValue{126}));
+
+  // This should affect srcBytes[] data too:
+  EXPECT_THAT(srcBytes[0], Eq(0xAF));
+  EXPECT_THAT(srcBytes[1], Eq(0x45));
+  EXPECT_THAT(srcBytes[2], Eq(0x7E));
+
+  // Can also mutate through rawBytes():
+  ref.rawBytes()[0] = 0x92;
+  ref.rawBytes()[1] = 0x44;
+  ref.rawBytes()[2] = 0x5A;
+
+  EXPECT_THAT(ref.status(),
+      Eq(bmmidi::Status::channelVoice(bmmidi::MsgType::kNoteOn,
+                                      bmmidi::Channel::index(2))));
+  EXPECT_THAT(ref.data1(), Eq(bmmidi::DataValue{68}));
+  EXPECT_THAT(ref.data2(), Eq(bmmidi::DataValue{90}));
+
+  EXPECT_THAT(srcBytes[0], Eq(0x92));
+  EXPECT_THAT(srcBytes[1], Eq(0x44));
+  EXPECT_THAT(srcBytes[2], Eq(0x5A));
+}
+
+TEST(MsgRef, WorksForMsg) {
+  // Control Change, channel 13, CC #1 (Mod Wheel), value 37.
+  bmmidi::Msg<3> ccCh13Mod37{
+      bmmidi::Status::channelVoice(bmmidi::MsgType::kControlChange,
+                                   bmmidi::Channel::index(12)),
+      bmmidi::controlToDataValue(bmmidi::Control::kModWheel),
+      bmmidi::DataValue{37}};
+  auto ref = ccCh13Mod37.ref();
+
+  // Read-only accessors are still available:
+  EXPECT_THAT(ref.type(), Eq(bmmidi::MsgType::kControlChange));
+  EXPECT_THAT(ref.status(),
+      Eq(bmmidi::Status::channelVoice(bmmidi::MsgType::kControlChange,
+                                      bmmidi::Channel::index(12))));
+  EXPECT_THAT(ref.data1(), Eq(bmmidi::DataValue{1}));
+  EXPECT_THAT(ref.data2(), Eq(bmmidi::DataValue{37}));
+  EXPECT_THAT(ref.numBytes(), Eq(3));
+
+  EXPECT_THAT(ref.rawBytes()[0], Eq(0xBC));
+  EXPECT_THAT(ref.rawBytes()[1], Eq(0x01));
+  EXPECT_THAT(ref.rawBytes()[2], Eq(0x25));
+
+  // Mutations:
+  ref.setData2(bmmidi::DataValue{126});
+  EXPECT_THAT(ref.status(),
+      Eq(bmmidi::Status::channelVoice(bmmidi::MsgType::kControlChange,
+                                      bmmidi::Channel::index(12))));
+  EXPECT_THAT(ref.data1(), Eq(bmmidi::DataValue{1}));
+  EXPECT_THAT(ref.data2(), Eq(bmmidi::DataValue{126}));
+
+  ref.setData1(bmmidi::DataValue{0x45});
+  EXPECT_THAT(ref.status(),
+      Eq(bmmidi::Status::channelVoice(bmmidi::MsgType::kControlChange,
+                                      bmmidi::Channel::index(12))));
+  EXPECT_THAT(ref.data1(), Eq(bmmidi::DataValue{69}));
+  EXPECT_THAT(ref.data2(), Eq(bmmidi::DataValue{126}));
+
+  ref.setStatus(
+      bmmidi::Status::channelVoice(bmmidi::MsgType::kPolyphonicKeyPressure,
+                                   bmmidi::Channel::index(15)));
+  EXPECT_THAT(ref.status(),
+      Eq(bmmidi::Status::channelVoice(bmmidi::MsgType::kPolyphonicKeyPressure,
+                                      bmmidi::Channel::index(15))));
+  EXPECT_THAT(ref.data1(), Eq(bmmidi::DataValue{69}));
+  EXPECT_THAT(ref.data2(), Eq(bmmidi::DataValue{126}));
+
+  // This should affect ccCh13Mod37 data too:
+  EXPECT_THAT(ccCh13Mod37.rawBytes()[0], Eq(0xAF));
+  EXPECT_THAT(ccCh13Mod37.rawBytes()[1], Eq(0x45));
+  EXPECT_THAT(ccCh13Mod37.rawBytes()[2], Eq(0x7E));
+
+  // Can also mutate through rawBytes():
+  ref.rawBytes()[0] = 0x92;
+  ref.rawBytes()[1] = 0x44;
+  ref.rawBytes()[2] = 0x5A;
+
+  EXPECT_THAT(ref.status(),
+      Eq(bmmidi::Status::channelVoice(bmmidi::MsgType::kNoteOn,
+                                      bmmidi::Channel::index(2))));
+  EXPECT_THAT(ref.data1(), Eq(bmmidi::DataValue{68}));
+  EXPECT_THAT(ref.data2(), Eq(bmmidi::DataValue{90}));
+
+  EXPECT_THAT(ccCh13Mod37.rawBytes()[0], Eq(0x92));
+  EXPECT_THAT(ccCh13Mod37.rawBytes()[1], Eq(0x44));
+  EXPECT_THAT(ccCh13Mod37.rawBytes()[2], Eq(0x5A));
+}
+
+// TODO: Add tests for MsgView and MsgRef referring to SysExMsg.
 
 }  // namespace
