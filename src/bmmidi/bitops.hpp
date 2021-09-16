3699fec9bd5b100363beb217b08883c536f23e02
Eric Barndollar <eric@EricWBarndollar.com>
2021-09-16 12:22:07 -0400

    Bit mask operations, for internal use

---
+++ src/bmmidi/bitops.hpp
@@ -0,0 +1,28 @@
+#ifndef BMMIDI_BITOPS_HPP
+#define BMMIDI_BITOPS_HPP
+
+#include <cstdint>
+
+namespace bmmidi {
+namespace internal {
+
+/** Returns only the bits in value where bitMask has 1 bits. */
+inline std::uint8_t getBits(std::uint8_t value, std::uint8_t bitMask) {
+  return (value & bitMask);
+}
+
+/**
+ * Returns oldValue with 1 bits in bitMask replaced by the corresponding bits in
+ * newValue.
+ */
+inline std::uint8_t setBits(std::uint8_t oldValue, std::uint8_t newValue, std::uint8_t bitMask) {
+  std::uint8_t result = oldValue;
+  result &= ~bitMask;  // Clear the bitMask bits.
+  result |= (newValue & bitMask);  // Replace with corresponding bits from newValue.
+  return result;
+}
+
+}  // namespace internal
+}  // namespace bmmidi
+
+#endif  // BMMIDI_BITOPS_HPP
---
+++ src/bmmidi/bitops_test.cpp
@@ -0,0 +1,60 @@
+#include "bmmidi/bitops.hpp"
+
+#include <cstdint>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+namespace bmmidi {
+namespace {
+
+using ::testing::Eq;
+using ::testing::IsFalse;
+using ::testing::IsTrue;
+
+TEST(GetBits, ReturnsOneBitsInMask) {
+  EXPECT_THAT(internal::getBits(0b0110'1011,
+                                0b0010'0111),
+                             Eq(0b0010'0011));
+
+  EXPECT_THAT(internal::getBits(0b0101'1010,
+                                0b0000'0000),
+                             Eq(0b0000'0000));
+
+  EXPECT_THAT(internal::getBits(0b0101'1010,
+                                0b1111'1111),
+                             Eq(0b0101'1010));
+
+  EXPECT_THAT(internal::getBits(0b1101'0110,
+                                0b1111'0000),
+                             Eq(0b1101'0000));
+
+  EXPECT_THAT(internal::getBits(0b1101'0110,
+                                0b0011'1111),
+                             Eq(0b0001'0110));
+}
+
+TEST(SetBits, ReplacesOnlyOneBitsInMask) {
+  EXPECT_THAT(internal::setBits(0b0110'1011,
+                                0b1111'0110,
+                                0b0000'1111),
+                             Eq(0b0110'0110));
+
+  EXPECT_THAT(internal::setBits(0b0110'1011,
+                                0b1111'0110,
+                                0b0101'0101),
+                             Eq(0b0111'1110));
+
+  EXPECT_THAT(internal::setBits(0b0110'1011,
+                                0b0000'1001,
+                                0b0000'1111),
+                             Eq(0b0110'1001));
+
+  EXPECT_THAT(internal::setBits(0b0101'1101,
+                                0b0010'0000,
+                                0b0110'0000),
+                             Eq(0b0011'1101));
+}
+
+}  // namespace
+}  // bmmidi
