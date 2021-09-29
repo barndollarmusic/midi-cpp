a6f81c49409f379542332ac3a694224b740af30d
Eric Barndollar <eric@EricWBarndollar.com>
2021-09-29 17:18:36 -0400

    Support SOURCE_SUBDIR (even though not needed yet).

--- cmake/BMMidiDeps.cmake
+++ cmake/BMMidiDeps.cmake
@@ -1,14 +1,23 @@
 include(FetchContent)
 
 ## Like FetchContent_MakeAvailable, but adds EXCLUDE_FROM_ALL when invoking
-## add_subdirectory().
+## add_subdirectory(). Accepts SOURCE_SUBDIR argument (which must be repeated,
+## since we don't want to access private FetchContent() state).
 macro(bmmidi_make_dep_available dep)
+  cmake_parse_arguments(ARG "" "SOURCE_SUBDIR" "" ${ARGN})
+
   string(TOLOWER "${dep}" lowerCaseDep)
   FetchContent_GetProperties(${dep})
 
   if(NOT ${lowerCaseDep}_POPULATED)
     FetchContent_Populate(${dep})
-    add_subdirectory("${${lowerCaseDep}_SOURCE_DIR}" "${${lowerCaseDep}_BINARY_DIR}"
+
+    set(srcDir "${${lowerCaseDep}_SOURCE_DIR}")
+    if(NOT "${ARG_SOURCE_SUBDIR}" STREQUAL "")
+      string(APPEND srcDir "/${ARG_SOURCE_SUBDIR}")
+    endif()
+
+    add_subdirectory("${srcDir}" "${${lowerCaseDep}_BINARY_DIR}"
         EXCLUDE_FROM_ALL)
   endif()
 endmacro()
