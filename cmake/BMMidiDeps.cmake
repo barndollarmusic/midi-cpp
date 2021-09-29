48be10f0509a1b600f65e73192dccf4191dc3e3d
Eric Barndollar <eric@EricWBarndollar.com>
2021-09-29 16:45:20 -0400

    Handle non-lowercase dep names correctly.

--- cmake/BMMidiDeps.cmake
+++ cmake/BMMidiDeps.cmake
@@ -3,10 +3,12 @@ include(FetchContent)
 ## Like FetchContent_MakeAvailable, but adds EXCLUDE_FROM_ALL when invoking
 ## add_subdirectory().
 macro(bmmidi_make_dep_available dep)
+  string(TOLOWER "${dep}" lowerCaseDep)
   FetchContent_GetProperties(${dep})
 
   if(NOT ${dep}_POPULATED)
     FetchContent_Populate(${dep})
-    add_subdirectory("${${dep}_SOURCE_DIR}" "${${dep}_BINARY_DIR}" EXCLUDE_FROM_ALL)
+    add_subdirectory("${${lowerCaseDep}_SOURCE_DIR}" "${${lowerCaseDep}_BINARY_DIR}"
+        EXCLUDE_FROM_ALL)
   endif()
 endmacro()
